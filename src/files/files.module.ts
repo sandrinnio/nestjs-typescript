56351237fc45ef3b21a1435f2b1e6f7f2c12b93f
sandrinnio <sandrokakashvili@gmail.com>
2021-09-26 22:24:32 +0400

    fix: s3

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { S3 } from 'aws-sdk';
+import { AwsModule } from '../aws/aws.module';
 import PublicFile from './entities/public-file.entity';
 import { FilesRepository } from './files.repository';
 import { FilesService } from './files.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PublicFile]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([PublicFile]),
+    AwsModule.forFeature(S3),
+    ConfigModule,
+  ],
   providers: [FilesService, FilesRepository],
   exports: [FilesService],
 })
